8bd29b92ab631cc71cc94e9a6114449aede46e11
ljven00 <jeanvenaldolouissaint@gmail.com>
2023-05-16 14:03:03 -0600

    Replace project by different test file I already wrote

---
+++ index.js
@@ -0,0 +1,35 @@
+
+// min-width: 990px
+// max-width: 1480px
+const root = $(":root");
+const section = $("section");
+const themeBtn = $("#theme");
+
+$(document).ready(function(){
+
+    section.on({
+        mouseenter: function(){
+            let id = $(this).attr("id");
+            let currentEl = $(`[href="#${id}"]`);
+            currentEl.addClass("active");
+        },
+        mouseleave: function(){
+            let id = $(this).attr("id");
+            let currentEl = $(`[href="#${id}"]`);
+            currentEl.removeClass("active");
+        }
+    });
+
+    themeBtn.click(function (){
+        root.toggleClass("dark");
+        if(root.attr("class") === "dark"){
+            $("#next-theme").text("Light");
+            $("#theme-icon").removeClass("fa-circle-half-stroke").addClass("fa-sun");
+        }
+        else{
+            $("#next-theme").text("Dark");
+            $("#theme-icon").removeClass("fa-sun").addClass("fa-circle-half-stroke");
+        }
+    });
+    
+});
