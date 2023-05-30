1a478e6590326caf9749f069d54a305b2333a842
ljven00 <jeanvenaldolouissaint@gmail.com>
2023-05-29 23:31:58 -0600

    Update certificates

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const themeBtn = $("#theme");
 const mobileTheme = $("#mobile-theme");
 const leftArrow = $(".fa-arrow-left");
 const rightArrow =$(".fa-arrow-right");
+const prevCertificate = $("#certificates .fa-arrow-left");
+const nextCertificate = $("#certificates .fa-arrow-right");
 const skill = $(".skill");
 const menu = $("#menu");
 let caretLeft = $("#caret-left");
+
+
+let currCertificate = 0;
+let currProject = 0;
 
 const certificateEl = {
     title: $("#certificates .title"),
@@ -21,9 +27,8 @@ const projectEl = {
     img: $("#projects .img")
 }
 
-certificateEl.title.text(certificates[0].name)
-certificateEl.desc.text(certificates[0].desc)
-certificateEl.img.attr("src", certificates[0].url)
+updateCertificate(currCertificate);
+
 
 $(document).ready(function(){
     
@@ -84,14 +89,48 @@ $(document).ready(function(){
         }
     });
 
+    nextCertificate.click(function() {   
+        if(currCertificate < certificates.length - 1){
+            currCertificate++;
+            updateCertificate(currCertificate);
+        }else if(currCertificate == certificates.length - 1){
+            updateCertificate(currCertificate);
+        }else{
+            return;
+        }
+    });
+
+    prevCertificate.click(function() {
+        if(currCertificate == 0){
+            updateCertificate(currCertificate);
+        }else if(currCertificate >= 1){
+            currCertificate--;
+            updateCertificate(currCertificate);
+        }else{
+            return;
+        }
+    });
+
 });
 
-class Certificate{
-    constructor(name, url, description){
-        this.name = name;
-        this.url = url;
-        this.description = description;
-    }
+/**
+ * update the content of the certificate element
+ * @param {int} n the current certificate index
+ */
+function updateCertificate(n){
+    certificateEl.title.text(certificates[n].name)
+    certificateEl.desc.text(certificates[n].desc)
+    certificateEl.img.attr("src", certificates[n].url)
+}
+
+/**
+ * update the content of the project element
+ * @param {int} n the current project index
+ */
+function updateProject(n){
+    projectEl.title.text(projects[n].name)
+    projectEl.desc.text(projects[n].desc)
+    projectEl.img.attr("src", projects[n].url)
 }
 
 class Projects{
